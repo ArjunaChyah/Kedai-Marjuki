6f7007d62e01c946a76b036204e6b8946eefe32c
Arjunaa <arjuna@marjukis.test>
2026-08-18 10:54:50 +0700

    Add new product photos for Es Teh and Es Jeruk

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -136,7 +136,7 @@ class DatabaseSeeder extends Seeder
                 'price' => 3000,
                 'stock' => 100,
                 'status' => 'available',
-                'image' => null,
+                'image' => 'produk/esteh.jpg',
             ],
             [
                 'name' => 'Es Jeruk',
@@ -146,7 +146,7 @@ class DatabaseSeeder extends Seeder
                 'price' => 5000,
                 'stock' => 80,
                 'status' => 'available',
-                'image' => null,
+                'image' => 'produk/esjeruk.jpg',
             ],
         ];
 
